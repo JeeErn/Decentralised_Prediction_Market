ddf69356178772d98e83700571043b82da016223
JeeErn <jee.ern88@gmail.com>
2020-10-08 18:13:06 +0800

    update deploy contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var PredictionMarket = artifacts.require("./PredictionMarket.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(PredictionMarket);
   deployer.deploy(SimpleStorage);
 };
