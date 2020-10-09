23179397a633672df3c23c702ac523344735a395
JeeErn <jee.ern88@gmail.com>
2020-10-09 17:26:24 +0800

    Add test case for create arbitrators

--- test/predictionmarket.js
+++ test/predictionmarket.js
@@ -34,4 +34,31 @@ contract("PredictionMarket", accounts => {
             assert(trader1.isValid, "created trader remains valid");
         };
     }); 
+
+    it("allows a entity to create a arbitrator account with default values", async () => {
+        assert(predictionMarketInstance.address != "");
+        await predictionMarketInstance.createArbitrator({from: accounts[2]});
+        arbitrator = await predictionMarketInstance.arbitrators(accounts[2]);
+        assert(arbitrator.isValid, "arbitrator is init to valid");
+        assert(arbitrator.trustworthiness, 100, "arbitrator trustworthiness score is init to 100");
+    });
+
+    it("throws an exception when an existing arbitrator creates a new account", async () => {
+        await predictionMarketInstance.createArbitrator({ from: accounts[3] });
+        arbitrator = await predictionMarketInstance.arbitrators(accounts[3]);
+
+        // First creation is accepted
+        assert(arbitrator.isValid, "arbitrator is init to valid");
+        assert(arbitrator.winScore, 100, "arbitrator trustworthiness score is init to 100");
+
+        // Try to create again
+        try {
+            await predictionMarketInstance.createArbitrator({ from: accounts[3] });
+        } catch (error) {
+            assert(error.message.indexOf("revert") >= 0, "error message must contain revert");
+        } finally {
+            arbitrator1 = await predictionMarketInstance.traders(accounts[1]);
+            assert(arbitrator1.isValid, "created arbitrator remains valid");
+        };
+    }); 
 })
