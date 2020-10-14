ab8bd6ecb8cf2c78a77cb50daf6ef357675491c5
JeeErn <jee.ern88@gmail.com>
2020-10-14 22:58:36 +0800

    Add selected arbitrators to create topic - check that selected addresses have arbitrator accounts - check that creator is not a selected arbitrator - Remove unnecessary "payable" tags - Add getter for selected arbitrators

--- contracts/PredictionMarket.sol
+++ contracts/PredictionMarket.sol
@@ -46,7 +46,7 @@ contract PredictionMarket {
 
     // Topics
     function createTopic(
-        string memory name, string memory description, bytes32[] memory options, uint256 expiryDate
+        string memory name, string memory description, bytes32[] memory options, uint256 expiryDate, address payable[] memory selectedArbitrators
     ) public payable
         {
         address payable creatorId = msg.sender;
@@ -57,8 +57,11 @@ contract PredictionMarket {
         // Must have value above 0 ETH for payment of creation bond
         require(msg.value > 0 ether);
 
+        // selected arbitrators must have arbitrator account and creator must not select himself as an arbitrator
+        require(areValidArbitrators(creatorId, selectedArbitrators));
+
         uint bondValue = msg.value;
-        Topic newTopic = new Topic(creatorId, name, description, options, bondValue, expiryDate);
+        Topic newTopic = new Topic(creatorId, name, description, options, bondValue, expiryDate, selectedArbitrators);
         address payable topicAddress = address(uint160(address(newTopic))); // to cast from address to address payable
         marketTopics[topicAddress] = newTopic;
         topicAddresses.push(topicAddress);
@@ -67,6 +70,17 @@ contract PredictionMarket {
 
     function getAllTopics() public view returns (address[] memory) {
         return topicAddresses;
+    }
+
+    function areValidArbitrators(address payable creatorId, address payable[] memory selectedArbitrators) internal view returns (bool) {
+        uint numSelectedArbitrators = selectedArbitrators.length;
+        for (uint i = 0; i < numSelectedArbitrators; i++) {
+            address arbitrator = selectedArbitrators[i];
+            if (!arbitrators[arbitrator].isValid || creatorId == arbitrator) {
+                return false;
+            }
+        }
+        return true;
     }
 
     // // TODO: To complete createTopic function when Topic contract is completed
@@ -80,32 +94,36 @@ contract PredictionMarket {
     // ===============================================================
     event TopicCreated(address _topicAddress);
 
-    function getCreatorAddress(address payable topicAddress) public view returns (address payable) {
+    function getCreatorAddress(address topicAddress) public view returns (address payable) {
         return marketTopics[topicAddress].topicCreator();
     }
 
-    function getName(address payable topicAddress) public view returns (string memory) {
+    function getName(address topicAddress) public view returns (string memory) {
         return marketTopics[topicAddress].name();
     }
 
-    function getDescription(address payable topicAddress) public view returns (string memory) {
+    function getDescription(address topicAddress) public view returns (string memory) {
         return marketTopics[topicAddress].description();
     }
 
-    function getOptions(address payable topicAddress) public view returns (bytes32[] memory) {
+    function getOptions(address topicAddress) public view returns (bytes32[] memory) {
         return marketTopics[topicAddress].getOptions();
     }
 
-    function getMarketCap(address payable topicAddress) public view returns (uint) {
+    function getMarketCap(address topicAddress) public view returns (uint) {
         return marketTopics[topicAddress].marketCap();
     }
 
-    function getCreatorBond(address payable topicAddress) public view returns (uint) {
+    function getCreatorBond(address topicAddress) public view returns (uint) {
         return marketTopics[topicAddress].creatorBond();
     }
 
-    function getExpiryDate(address payable topicAddress) public view returns (uint256) {
+    function getExpiryDate(address topicAddress) public view returns (uint256) {
         return marketTopics[topicAddress].expiryDate();
+    }
+
+    function getSelectedArbitrators(address topicAddress) public view returns (address payable[] memory) {
+        return marketTopics[topicAddress].getArbitrators();
     }
 
     function getCurrentBalance() public view returns (uint) {
