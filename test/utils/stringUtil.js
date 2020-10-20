ab4504231763e177d9fad0c1fb0ad38efec067f6
JeeErn <jee.ern88@gmail.com>
2020-10-20 23:15:55 +0800

    Change string util functions - Allow both array of strings and single string

--- test/utils/stringUtil.js
+++ test/utils/stringUtil.js
@@ -1,9 +1,15 @@
 const stringToBytes = (options) => {
-    return options.map(option => web3.utils.asciiToHex(option));
+    if (Array.isArray(options)) {
+        return options.map(option => web3.utils.asciiToHex(option));
+    }
+    return web3.utils.asciiToHex(options);
 }
 
 const bytesToString = (options) => {
-    return options.map(option => web3.utils.hexToUtf8(option));
+    if (Array.isArray(options)) {
+        return options.map(option => web3.utils.hexToUtf8(option));
+    }
+    return web3.utils.hexToUtf8(options);
 }
 
 exports.stringToBytes = stringToBytes;
